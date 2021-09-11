9af99b96ddf7b555341001e274841f825a59ce3b
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-11 13:59:59 -0400

    Update Main.java

--- Rotate_Min/src/Rotate_Min/Main.java
+++ Rotate_Min/src/Rotate_Min/Main.java
@@ -1,4 +1,21 @@
 package Rotate_Min;
 
+/*This class is Main class. In this class, users can change the input numbers in hardcoded code line.
+  This minimum value search is using the binary search.
+  Thus, we need to sort the array before searching the middle point, minimum and maximum value in the array.
+  
+  Author: Hyejin Kim
+  Date: 9/12/2021 */
+
+public class Main {
+    public static void main(String[] args){
+        Rotate_Min RM = new Rotate_Min();
+        int[] userInputs = {4,5,6,7,0,1,2}; //User can change the hardcoded input values here.
+        int minimumOutput = 0;
+        minimumOutput = RM.minimumValue(userInputs);
+        System.out.println("The minimum value is " + minimumOutput);
+    }
+}package Rotate_Min;
+
 public class Main {
 }
